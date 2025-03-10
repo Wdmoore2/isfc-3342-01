a9b01932e9cc95c070820b6bc9369092329e4dae
Will Moore <wdmoore2@ualr.edu>
2025-03-10 17:50:09 +0000

    Added Awesome font for the the link pages, removed css ul bullets

--- home-page/assets/js/app.js
+++ home-page/assets/js/app.js
@@ -1,7 +1,1 @@
-const themeToggle = document.getElementById("theme-toggle");
-if (themeToggle) {
-  themeToggle.addEventListener("click", () => {
-    document.documentElement.classList.toggle("dark-mode");
-    document.documentElement.classList.toggle("light-mode");
-  });
-}
+
